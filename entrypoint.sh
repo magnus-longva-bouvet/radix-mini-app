48034e3e55df286ed0591a732adc19a00daf5127
Magnus Longva (IT MSS LID) <magnus@longva.email>
2022-08-12 13:37:10 +0200

    different ports per component

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-while true; do printf 'HTTP/1.1 200 OK\n\n%s' "hello" | netcat -l -N 8080; done
+while true; do printf 'HTTP/1.1 200 OK\n\n%s' "hello" | netcat -l -N "${public_port}"; done
